187a5f0a816f6045be06cb5966c5717896cd9feb
jabia_000 <jabia_000@JABianchi-Flip.05M200.nycboe.org>
2017-01-18 18:34:59 -0500

    commiting to test

--- src/org/usfirst/frc/team1660/robot/Robot.java
+++ src/org/usfirst/frc/team1660/robot/Robot.java
@@ -3,13 +3,16 @@ package org.usfirst.frc.team1660.robot;
 
 import org.usfirst.frc.team1660.robot.HKdrive;
 import org.usfirst.frc.team1660.robot.GripPipeline;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import.edu.wpi.first.wpilibj.networktables.NetworkTable;
+
 import com.ctre.CANTalon;
 import com.kauailabs.navx;
 
@@ -18,10 +21,10 @@ public class Robot extends SampleRobot {
 	HKdrive robotDrive;
 	NetworkTable table;
 	
-	  //DECLARING JOYSTICK VARIABLES   -jamesey
-		int FORWARDBACKWARD_AXIS = 1; //Left joystick up and down
-		int TURNSIDEWAYS_AXIS = 4; //Right joystick side to side
-		int STRAFE_AXIS = 0; //Left joystick side to side
+  //DECLARING JOYSTICK VARIABLES   -jamesey
+	final int FORWARDBACKWARD_AXIS = 1; //Left joystick up and down
+	final int TURNSIDEWAYS_AXIS = 4; //Right joystick side to side
+	final int STRAFE_AXIS = 0; //Left joystick side to side
 				
 	// Channels for the wheels
 	final int kFrontLeftChannel = 1;
@@ -44,23 +47,40 @@ public class Robot extends SampleRobot {
 		
 		robotDrive.setExpiration(0.1);
 	}
-
+	
+	
+	/* This function is run when the robot is first started up and should be
+	  used for any initialization code. */
 	public void robotInit() {
 		
 		table = NetworkTable.getTable("marly");
 		
 	}
 	
-	public void autonomous(){
-		
+	/* This function is run once each time the robot enters autonomous mode */
+	public void autonomousInit() {
+		timer.reset();
+		timer.start();
+	}
+
+	/* This function is called periodically during autonomous */
+	public void autonomousPeriodic() {
+		// Drive for 2 seconds
+		if (timer.get() < 2.0) {
+			myRobot.drive(-0.5, 0.0); // drive forwards half speed
+		} else {
+			myRobot.drive(0.0, 0.0); // stop robot
+		}
 	}
 	
-	
-	/**
-	 * Runs the motors with Mecanum drive.
-	 */
-	@Override
-	public void operatorControl() {
+	/* This function is called once each time the robot enters tele-operated mode */
+	public void teleopInit() {
+		
+	}
+
+	/* This function is called periodically during operator control */
+	public void teleopPeriodic() {
+		
 		robotDrive.setSafetyEnabled(true);
 		double x = 0;
 		double y = 0;
@@ -83,7 +103,20 @@ public class Robot extends SampleRobot {
 			
 		}
 	}
+
+
+	public void operatorControl() {
+
+	}
 	
+	
+	/* This function is called periodically during test mode */
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+	
+	
+	/* CUSTOM FUNCTIONS */
 	
 	//MOVE DRIVETRAIN WITH XBOX360 JOYSTICKS -Matthew
 	public void checkJoystick()
