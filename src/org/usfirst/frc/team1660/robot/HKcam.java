a84928190bea03b972d06054d0d3418aca14668e
jabia_000 <jabia_000@JABianchi-Flip>
2017-01-29 17:51:50 -0500

    Merged code emailed from Jamesey from several laptops (gear collection and Relays).  Formatted code with headings for readability.

---
+++ src/org/usfirst/frc/team1660/robot/HKcam.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team1660.robot;
+
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Rect;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import org.usfirst.frc.team1660.robot.GripPipeline;
+
+public class HKcam {
+	
+	
+	
+	
+	
+	
+}
